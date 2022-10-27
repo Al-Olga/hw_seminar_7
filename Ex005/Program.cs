4de2d31d1e57bf35a8d405830c021f4643ad44a2
Olga_A <zagaes2@rambler.ru>
2022-10-28 00:57:48 +0300

    correct program 005

--- Ex005/Program.cs
+++ Ex005/Program.cs
@@ -3,9 +3,8 @@
 int SumElement (int[,] array)
 {
     int sum = 0;
-    for (int i=0; i<array.GetLength(0); i++)
-        for (int j=0; j<array.GetLength(1); j++)
-            if (i==j) sum=sum+array[i,j];
+    for (int i=0; (i<array.GetLength(0) & (i<array.GetLength(1))); i++)
+       sum=sum+array[i,i];
     return sum;
 }
 
