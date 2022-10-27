eb70b3bf00bfd32746b84cb800595544f147471e
Olga_A <zagaes2@rambler.ru>
2022-10-27 12:05:25 +0300

    Ex004 added

---
+++ Ex004/Program.cs
@@ -0,0 +1,53 @@
+﻿// В двумерном массиве показать позиции числа, 
+// заданного пользователем или указать, что такого элемента нет
+void FindElement(int[,] array, int element)
+{
+    int count=0;
+    for (int i=0; i<array.GetLength(0); i++)
+        for (int j=0; j<array.GetLength(1); j++)
+            if (array[i,j]==element)
+            {
+                count++;
+                Console.WriteLine($"Число {element} найдено в строке {i+1}, столбце {j+1}");
+            }
+    if (count==0)    
+        Console.WriteLine($"Число {element} не найдено");
+    else 
+        Console.WriteLine($"Число найдено {count} раз");
+}
+
+void EnterArray (int[,] array, int min, int max)
+{
+    for (int i=0; i<array.GetLength(0); i++)
+        for (int j=0; j<array.GetLength(1); j++)
+            array[i,j]=new Random().Next(min,max);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i=0; i<array.GetLength(0); i++)
+    {
+        for (int j=0; j<array.GetLength(1); j++)
+            Console.Write($"{array[i,j]} ");
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество строк: ");
+int m=int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите количество столбцов: ");
+int n=int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите минимальный элемент массива: ");
+int min=int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите максимальный элемент массива: ");
+int max=int.Parse(Console.ReadLine() ?? "0");
+
+int[,] array = new int[m,n];
+
+EnterArray(array, min, max); // заполняем массив элементами
+Console.WriteLine("Введенный массив:");
+PrintArray(array);
+
+Console.Write("Введите число для его поиска в массиве: ");
+int number=int.Parse(Console.ReadLine() ?? "0");
+FindElement(array, number);
