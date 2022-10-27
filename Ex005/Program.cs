73c3e748b9c493bcc97e67b0a3f68e37f7127204
Olga_A <zagaes2@rambler.ru>
2022-10-27 12:55:16 +0300

    Ex005 added

---
+++ Ex005/Program.cs
@@ -0,0 +1,44 @@
+﻿// В матрице чисел найти сумму элементов главной диагонали
+
+int SumElement (int[,] array)
+{
+    int sum = 0;
+    for (int i=0; i<array.GetLength(0); i++)
+        for (int j=0; j<array.GetLength(1); j++)
+            if (i==j) sum=sum+array[i,j];
+    return sum;
+}
+
+void EnterArray (int[,] array, int min, int max)
+{
+    for (int i=0; i<array.GetLength(0); i++)
+        for (int j=0; j<array.GetLength(1); j++)
+            array[i,j]=new Random().Next(min,max);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i=0; i<array.GetLength(0); i++)
+    {
+        for (int j=0; j<array.GetLength(1); j++)
+            Console.Write($"{array[i,j]} ");
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество строк: ");
+int m=int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите количество столбцов: ");
+int n=int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите минимальный элемент массива: ");
+int min=int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите максимальный элемент массива: ");
+int max=int.Parse(Console.ReadLine() ?? "0");
+
+int[,] array = new int[m,n];
+
+EnterArray(array, min, max); // заполняем массив элементами
+Console.WriteLine("Введенный массив:");
+PrintArray(array);
+
+Console.WriteLine($"сумма элементов главной диагонали = {SumElement (array)}");
