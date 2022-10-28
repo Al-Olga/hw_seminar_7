9c736a083fde3ad7c636d681c0886b524227d511
Olga_A <zagaes2@rambler.ru>
2022-10-28 03:24:03 +0300

    correct program 004

--- Ex004/Program.cs
+++ Ex004/Program.cs
@@ -1,19 +1,33 @@
 ﻿// В двумерном массиве показать позиции числа, 
 // заданного пользователем или указать, что такого элемента нет
-void FindElement(int[,] array, int element)
+
+// обнуляем массив для временного хранения индексов найденного элемента
+void ObnulArrayFind(int[] array)
 {
-    int count=0;
-    for (int i=0; i<array.GetLength(0); i++)
-        for (int j=0; j<array.GetLength(1); j++)
+    for (int i=0; i<array.Length; i++)
+        array[i]=0;
+}
+
+int[] FindElement(int[,] array, int element, int strok, int stolb, int flag)
+{
+    int [] array_find = new int[3];
+    ObnulArrayFind(array_find);
+    if (flag==0) stolb=0; // если это первый "проход", то принудительно на нулевой индекс столбца
+    for (int i=strok; i<array.GetLength(0); i++) // поиск начинаем с индекса строки найденного элемента
+    {
+        for (int j=stolb; j<array.GetLength(1); j++) // поиск начинаем со следующего индекса столбца найденного элемента
             if (array[i,j]==element)
             {
-                count++;
-                Console.WriteLine($"Число {element} найдено в строке {i+1}, столбце {j+1}");
+                array_find[0]=i; // сохраняем позицию в строке
+                array_find[1]=j; // сохраняем позицию в столбце
+                array_find[2]=1; // "флаг", что элемент найден
+                return array_find;
             }
-    if (count==0)    
-        Console.WriteLine($"Число {element} не найдено");
-    else 
-        Console.WriteLine($"Число найдено {count} раз");
+        stolb=0;
+    }
+    array_find[0]=array.GetLength(0);
+    array_find[1]=array.GetLength(1);
+    return array_find;
 }
 
 void EnterArray (int[,] array, int min, int max)
@@ -48,6 +62,22 @@ EnterArray(array, min, max); // заполняем массив элемента
 Console.WriteLine("Введенный массив:");
 PrintArray(array);
 
+// создаем массив для временного хранения индексов найденного элемента
+int[] ArrayFind = new int[3];
 Console.Write("Введите число для его поиска в массиве: ");
 int number=int.Parse(Console.ReadLine() ?? "0");
-FindElement(array, number);
+int count=0;
+while (ArrayFind[0]<array.GetLength(0) & ArrayFind[1]<array.GetLength(1))
+{
+    // в функцию передаем: массив, число для поиска, 
+    // индексы с которых начинать поиск (для индекса столбца 
+    // за начало принят индекс следующего за найденным)
+    ArrayFind=FindElement(array, number, ArrayFind[0], ArrayFind[1]+1, count);
+    if (ArrayFind[2]==1)
+    {
+        Console.WriteLine($"Число {number} найдено в строке {ArrayFind[0]+1}, столбце {ArrayFind[1]+1}");
+        count++;
+    }
+}
+if (count==0)
+        Console.WriteLine($"Число {number} не найдено");
